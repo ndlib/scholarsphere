ddbd5499ac6d5ef806a53d0055f7182df72da6dd
Don Brower <dbrower@nd.edu>
2012-11-15 15:13:22 -0500

    Fix loading of resque configuration

--- config/initializers/resque_config.rb
+++ config/initializers/resque_config.rb
@@ -17,7 +17,7 @@ require 'socket'
 rails_root = ENV['RAILS_ROOT'] || "#{File.dirname(__FILE__)}/../.."
 rails_env = ENV['RAILS_ENV'] || 'development'
 
-config = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)['resque'].with_indifferent_access
+config = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)[rails_env].with_indifferent_access
 Resque.redis = Redis.new(host: config[:host], port: config[:port], thread_safe: true)
 
 Resque.inline = rails_env == 'test'
