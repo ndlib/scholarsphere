fe8492dcd00afe9f6a9475ac356c1d83f1b005da
Don Brower <dbrower@nd.edu>
2012-11-28 11:01:25 -0500

    Changes from the production machine

--- start-pool.sh
+++ start-pool.sh
@@ -3,4 +3,4 @@
 # Starts the resque pool on the dl-vecnet server. It assumes
 # there is an nginx reverse proxy at port 80.
 
-RAILS_ENV=dlvecnet bundle exec resque-pool -E dlvecnet
+RAILS_ENV=dlvecnet bundle exec resque-pool --daemon --pidfile resque-pool.pid --stdout log/resque-pool.stdout.log --stderr log/resque-pool.stderr.log --environment dlvecnet
--- start-server.sh
+++ start-server.sh
@@ -3,6 +3,7 @@
 # Starts the server on the dl-vecnet server. It assumes
 # there is an nginx reverse proxy at port 80
 
-LISTEN_PORTS="-l 127.0.0.1:3001"
+#LISTEN_PORTS="-l 127.0.0.1:3001"
+export RAILS_ROOT=/home/dbrower/dlvecnet
 
-RAILS_ENV=dlvecnet bundle exec unicorn $LISTEN_PORTS
+RAILS_ENV=dlvecnet bundle exec unicorn -d -E dlvecnet -c $RAILS_ROOT/unicorn.rb
