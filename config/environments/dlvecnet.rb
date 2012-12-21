cc6efefbeec71972872c19b76612c78d9ca68118
Don Brower <dbrower@nd.edu>
2012-12-21 09:51:25 -0500

    Adjust configuration to match what is deployed

--- config/environments/dlvecnet.rb
+++ config/environments/dlvecnet.rb
@@ -27,7 +27,13 @@ ScholarSphere::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   #config.action_view.debug_rjs             = false
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
+
+  # Specifies the header that your server uses for sending files
+  #config.action_dispatch.x_sendfile_header = "X-Sendfile"
+
+  # For nginx:
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
@@ -38,11 +44,17 @@ ScholarSphere::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  # Do not compress assets
-  config.assets.compress = false
+  # Compress assets
+  config.assets.compress = true
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+  # Generate digests for assets URLs.
+  config.assets.digest = true
 
   # adjust to where fits is installed
   config.fits_path = '/opt/fits-0.6.1/fits.sh'
