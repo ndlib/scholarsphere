6a25c10e2f245dc18741bea72762ad3d648a71a6
Don Brower <dbrower@nd.edu>
2013-01-03 16:03:15 -0500

    Make icons on dashboard smaller

--- app/assets/javascripts/scholarsphere.js
+++ app/assets/javascripts/scholarsphere.js
@@ -192,10 +192,10 @@ $(function() {
       var docId = a[1];
       $("#detail_"+docId).toggle();
       if( $("#detail_"+docId).is(":hidden") ) {
-        $("#expand_"+docId).attr("class", "icon-plus icon-large");
+        $("#expand_"+docId).attr("class", "icon-plus");
       }
       else {
-        $("#expand_"+docId).attr("class", "icon-minus icon-large");
+        $("#expand_"+docId).attr("class", "icon-minus");
       }
     }
     return false;
